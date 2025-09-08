877bc875cf4f85080c38bf98380885353ea96801
drocpdp <davidreynon@gmail.com>
2025-09-08 15:54:15 +0200

    product grid

---
+++ vite-project/src/data/products.tsx
@@ -0,0 +1,70 @@
+const productslist = [
+        {
+            'id': 1,
+            'name': 'North Face Hat',
+            'category': 'HATS',
+            'price': 25.00
+        },
+        {
+            'id': 2,
+            'name': 'Tottenham Hat',
+            'category': 'HATS',
+            'price': 30.00 
+        },
+        {
+            'id': 3,
+            'name': 'Nike Beanie',
+            'category': 'HATS',
+            'price': 35.00 
+        },        
+        {
+            'id': 4,
+            'name': 'Generic Wool Hat',
+            'category': 'HATS',
+            'price': 21.95 
+        },        
+        {
+            'id': 5,
+            'name': 'Golfers Cap',
+            'category': 'HATS',
+            'price': 15.00 
+        },        
+        {
+            'id': 6,
+            'name': 'Wool Earflap Hat',
+            'category': 'HATS',
+            'price': 40.00
+        },        
+        {
+            'id': 7,
+            'name': 'Heung Min Son Shirt',
+            'category': 'SHIRTS',
+            'price': 30.00 
+        },        
+        {
+            'id': 8,
+            'name': 'Plain WHite T-Shirt',
+            'category': 'SHIRTS',
+            'price': 10.00 
+        },        
+        {
+            'id': 9,
+            'name': 'Plain Grey T-Shirt',
+            'category': 'SHIRTS',
+            'price': 10.00 
+        },        
+        {
+            'id': 10,
+            'name': 'Polo Collar T-Shirt, Yellow',
+            'category': 'SHIRTS',
+            'price': 34.99 
+        },                
+        {
+            'id': 11,
+            'name': 'Polo Collar T-Shirt, Black',
+            'category': 'SHIRTS',
+            'price': 35.99 
+        },                
+    ]
+
+export default productslist;
--- vite-project/src/routes/products/products.component.tsx
+++ vite-project/src/routes/products/products.component.tsx
@@ -1,8 +1,35 @@
+import productslist from "../../data/products";
 
 const Products = () => {
     return (
         <>
             Products
+            <div className="grid grid-cols-4 gap-4">
+                {productslist.map((item) => (
+                    <div className="border rounded-xl border-width:1px p-3">
+                        <div>
+                            {item.id}
+                        </div>                                                
+                        <div>
+                            {item.name}
+                        </div>
+                        <div>
+                            {item.category}
+                        </div>
+                        <div>
+                            {item.price}
+                        </div>
+                        <div>
+                            <button 
+                                className="px-4 py-2 bg-blue-400 text-white rounded-lg hover:bg-blue-600"
+                            >
+                            Add to Cart
+                            </button>
+                        </div>
+                    </div>
+                    )
+                )}
+            </div>
         </>
     )
 }
